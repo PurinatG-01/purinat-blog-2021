0645ff2a778617b5ec51871a8f08c28d23acaa05
PurinatG-01 <fame3js49@gmail.com>
2021-04-05 08:05:21 +0700

    feat: add expense-sv project to display

--- constants/Projects.js
+++ constants/Projects.js
@@ -1,7 +1,9 @@
 const Projects = [
     { name: "AiRadar", link: "https://github.com/PurinatG-01/Senior-AirQuality-IoT", logo: "/image/AiRadar-logo.jpg", description: "ICT Mahidol Senior project." },
     { name: "Vue", link: undefined, logo: "/image/vue-logo.png", description: "ICT Mahidol Senior project." },
-    { name: "React", link: undefined, logo: "/image/react-logo.png", description: "ICT Mahidol Senior project." }
+    { name: "React", link: undefined, logo: "/image/react-logo.png", description: "ICT Mahidol Senior project." },
+    { name: "Expense-sv", link: "https://purinatg-01.github.io/expense-sv/", logo: "/image/svelte-logo.png", description: "My personal svelte project with the expense recorder app idea. Th eproject was building with svelte and taildwind css. I also use Firebase as a auth and cloud store service." }
+
 ]
 
 export default Projects
