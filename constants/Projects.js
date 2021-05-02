5e956215e9685ca37d5749658d60b63b6b41aa5c
PurinatG-01 <fame3js49@gmail.com>
2021-05-03 03:36:51 +0700

    feat: add new bot project

--- constants/Projects.js
+++ constants/Projects.js
@@ -1,9 +1,7 @@
 const Projects = [
     { name: "AiRadar", link: "https://github.com/PurinatG-01/Senior-AirQuality-IoT", logo: "/image/AiRadar-logo.jpg", description: "ICT Mahidol Senior project." },
-    { name: "Vue", link: undefined, logo: "/image/vue-logo.png", description: "ICT Mahidol Senior project." },
-    { name: "React", link: undefined, logo: "/image/react-logo.png", description: "ICT Mahidol Senior project." },
-    { name: "Expense-sv", link: "https://purinatg-01.github.io/expense-sv/", logo: "/image/svelte-logo.png", description: "My personal svelte project with the expense recorder app idea. Th eproject was building with svelte and taildwind css. I also use Firebase as a auth and cloud store service." }
-
+    { name: "Expense-sv", link: "https://purinatg-01.github.io/expense-sv/", logo: "/image/svelte-logo.png", description: "My personal svelte project with the expense recorder app idea. Th eproject was building with svelte and taildwind css. I also use Firebase as a auth and cloud store service." },
+    { name: "Discord Git Bot Distributor", link: "https://replit.com/@PurinatG01/Git-Log-Distributor", logo: "/image/github-logo.png", description: "Discord git distributor in personal server in python"}
 ]
 
 export default Projects
